387a7604b35bb240e36a2e9420f3e83850f7a421
Gabriel Sena <gabesenacom@gmail.com>
2021-05-15 19:42:13 -0300

    Add starting configs

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  //npx webpack serve
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
